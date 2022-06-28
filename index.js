1f07cf97cdd478a7533263765286bccb2f0ae42a
Griffin Bauer <griffinrbauer@icloud.com>
2022-06-27 20:23:20 -0500

    stuff

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ import axios from "axios";
 
 // Configuration
 let pollingRate = 1000 // DO NOT CHANGE THIS! This is meant to be changed to the time remaining before the next song is played.
-let artistName = "JOURNEY" // The artist to detect.
+let artistName = "The Outfield" // The artist to detect.
 let apiKey = "kglk" // LDRHub API Key. Usually the station's callsign.
 let matches = 0
 // Make UI
 import blessed from 'blessed'
 import contrib from 'blessed-contrib'
+import fs from 'fs'
+import jimp from 'jimp'
+
 let screen = blessed.screen()
-let grid = new contrib.grid({rows: 1, cols: 2, screen: screen})
+let grid = new contrib.grid({rows: 2, cols: 2, screen: screen})
 
 var log = grid.set(0, 0, 1, 1, contrib.log,
     { style:
@@ -31,7 +34,8 @@ var table = grid.set(0, 1, 1, 1, contrib.table, { keys: true
     , columnSpacing: 10 //in chars
     , columnWidth: [16, 12, 12] /*in chars*/ })
 
-contrib.gauge()
+var markdown = grid.set(1, 0, 1, 1, contrib.markdown)
+
 //allow control the table with the keyboard
 table.focus()
 
@@ -56,31 +60,38 @@ log.log(`\x1b[36mStation: \x1b[1m${apiKey}`);
 // Function for looping
 function loop() {
     setTimeout(async () => {
-        log.log('Fetching artist...')
+        log.log('\x1b[90mFetching artist...')
         await axios.get(`https://api.ldrhub.com/2/?key=${apiKey}&method=Station.Engage.NowPlaying`)
-        .then((r) => {
+        .then(async (r) => {
+            // Not needed but makes this all cleaner
+            let now_playing = r.data["Station.Engage.NowPlaying"].now_playing
+
             // Check if now_playing is null (an ad may be playing, or the station is processing new player data)
-            if (r.data["Station.Engage.NowPlaying"].now_playing === null) {
+            if (now_playing === null) {
                 log.log('\x1b[33mAn ad is playing, or the station is still processing. Waiting 15 seconds...')
+                markdown.setMarkdown(`# Currently Playing Song \n\x1b[5m\x1b[33mStation is processing`)
                 // Wait 10 more seconds before retrying
                 pollingRate = 15000
                 return loop()
             }
     
             // If we're at this point; compare playing artist to target.
-            if (r.data["Station.Engage.NowPlaying"].now_playing.artist === artistName) {
+            if (now_playing.artist === artistName) {
                 log.log(`\x1b[32mMatch detected! ${artistName} is currently playing on ${apiKey}!`);
-                table.setData(
+                await table.setData(
                     { headers: ['Artist', 'Matches']
                     , data:
-                        [ [artistName, matches++]]})
+                        [ [artistName, matches+1]]})
             } else {
-                log.log(`\x1b[33m${artistName} is not playing on ${apiKey}; ${r.data["Station.Engage.NowPlaying"].now_playing.artist} is.`);
+                log.log(`\x1b[33m${artistName} is not playing on ${apiKey}; ${now_playing.artist} is.`);
             }
-    
+            
+            // Set the details box.
+            markdown.setMarkdown(`# Currently Playing Song \n${now_playing.title} - ${now_playing.artist} in ${now_playing.album_name}`)
+            
             // Wait for the song to finish playing, and then scan again when the next one starts.
-            log.log(`\x1b[35mWaiting ${r.data["Station.Engage.NowPlaying"].now_playing.seconds_left * 1000} ms for the song to finish playing.`)
-            pollingRate = r.data["Station.Engage.NowPlaying"].now_playing.seconds_left * 1000
+            log.log(`\x1b[35mWaiting ${now_playing.seconds_left * 1000} ms for the song to finish playing.`)
+            pollingRate = now_playing.seconds_left * 1000
             loop()
         })
         .catch((e) => {
