cf8fc5ba6ea79d8a4cb3ec20883b46f787bc08b6
Griffin <griffin@edforma.app>
2023-11-29 13:00:23 -0600

    basic database interactions (not final)

--- index.ts
+++ index.ts
@@ -1,23 +1,35 @@
-import secrets from './secrets.json'
+import config from './config.json'
 import axios from 'axios'
 import { MongoClient } from 'mongodb'
+
 // Initialize MongoDB connection
-let client = new MongoClient(secrets.mongoConnectionString);
+let client = new MongoClient(config.database.mongoConnectionString);
 client.connect();
-let db = client.db('radio');
+let db = client.db('jj').collection('kglk');
 
 
 axios.request({
     url: 'https://quuit.com/quu/mobile/qipplaylist',
     params: {
-        stationid: 444,
+        stationid: config.tracking.stationId,
         type: 'json'
     }
 }).then((res) => {
-    res.data.playlist.forEach((item: any) => {
+    res.data.playlist.forEach(async (item: any) => {
         let readableDate = new Date(item.start).toUTCString();
-        console.log(`${item.title} - ${item.artist}`)
-        console.log(`${readableDate}\n`);
+
+        if (await db.findOne({ id: item.playlistid })) {
+            return console.log(`Not adding ${item.title}`);
+        } else {
+            console.log(`Adding ${item.title} to database`);
+            return await db.insertOne({
+                id: item.playlistid,
+                title: item.title,
+                artist: item.artist,
+                readableDate: readableDate
+            });
+        }
+
     });
 
     console.log(`Total: ${res.data.playlist.length} songs`);
