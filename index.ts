ca9444d8f188a6abd15a7376ecfc7252c0dce0ed
Griffin <griffin@edforma.app>
2023-11-29 11:35:23 -0600

    Basic playlist downloading

--- index.ts
+++ index.ts
@@ -1,1 +1,30 @@
-console.log("Hello via Bun!");
+import secrets from './secrets.json'
+import axios from 'axios'
+import { MongoClient } from 'mongodb'
+// Initialize MongoDB connection
+let client = new MongoClient(secrets.mongoConnectionString);
+client.connect();
+let db = client.db('radio');
+
+
+axios.request({
+    url: 'https://quuit.com/quu/mobile/qipplaylist',
+    params: {
+        stationid: 444,
+        type: 'json'
+    }
+}).then((res) => {
+    res.data.playlist.forEach((item: any) => {
+        let readableDate = new Date(item.start).toUTCString();
+        console.log(`${item.title} - ${item.artist}`)
+        console.log(`${readableDate}\n`);
+    });
+
+    console.log(`Total: ${res.data.playlist.length} songs`);
+})
+
+
+
+// setInterval(() => {
+//     console.log('Pulling playlist');
+// }, 1000)
