4986958a6a939b86498de657fc8d68f057d796dc
Griffin <griffin@edforma.app>
2023-12-01 12:14:59 -0600

    nicer logging

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import config from './config.json'
 import axios from 'axios'
 import { MongoClient } from 'mongodb'
+import chalk from 'chalk';
 
 // Initialize MongoDB connection
 let client = new MongoClient(config.database.mongoConnectionString);
@@ -15,7 +16,7 @@ async function pullData() {
             type: 'json'
         }
     }).then(async (res) => {
-        console.log(`got ${res.data.playlist.length} songs back from quuit`);
+        console.log(chalk.grey(`got ${res.data.playlist.length} songs back from quuit`));
 
         await res.data.playlist.forEach(async (item: any) => {
             let readableDate = new Date(item.start).toUTCString();
@@ -25,10 +26,10 @@ async function pullData() {
                 // Song exists in MongoDB, but is this a new playtime?
                 if (dbEntry.playtimes.includes(readableDate)) {
                     // We've seen this one before, skip it
-                    return console.log(`familiar playtime for ${item.title} (${item.playlistid})`);
+                    return console.log(chalk.grey(`familiar playtime for "${item.title}" (${item.playlistid})`));
                 } else {
                     // This is a brand new occourance, add it to the song's document
-                    console.log(`new playtime for ${item.title} (${item.playlistid}): ${readableDate}`)
+                    console.log(chalk.greenBright(`spotted "${item.title}" (${item.playlistid}): ${readableDate}`))
                     return db.updateOne(
                         { id: item.playlistid },
                         { $push: { playtimes: readableDate } },
@@ -37,7 +38,7 @@ async function pullData() {
 
             } else {
                 // Song doesn't exist in MongoDB, so add it along with this playtime
-                console.log(`new song ${item.title} (${item.playlistid})`);
+                console.log(chalk.yellowBright(`discovered "${item.title}" (${item.playlistid})`));
                 return await db.insertOne({
                     id: item.playlistid,
                     title: item.title,
