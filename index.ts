a788a19db5e5b40d0791d9733531db9cfe056600
Griffin <griffin@edforma.app>
2024-01-07 12:00:20 -0600

    moment.js!

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import config from './config.json'
 import axios from 'axios'
 import { MongoClient } from 'mongodb'
 import chalk from 'chalk';
+import moment from 'moment';
 
 let collection = config.database.useStationIdAsCollectionName ? config.tracking.stationId.toString() : config.tracking.callsign;
 
@@ -29,14 +30,14 @@ async function pullData() {
         console.log(chalk.grey(`got ${res.data.playlist.length} songs back from quuit`));
 
         res.data.playlist.forEach(async (item: any) => {
-            let readableDate = new Date(item.start).toUTCString();
+            let readableDate = moment.utc(item.start).format('YYYY-MM-DD HH:mm:ss a');
             let dbEntry = await db.findOne({ id: item.playlistid });
 
             if (dbEntry) {
                 // Song exists in MongoDB, but is this a new playtime?
                 if (dbEntry.playtimes.includes(readableDate)) {
                     // We've seen this one before, skip it
-                    return console.log(chalk.grey(`familiar playtime for "${item.title}" (${item.playlistid})`));
+                    return console.log(chalk.grey(`familiar playtime for "${item.title}" (${item.playlistid}): ${readableDate}`));
                 } else {
                     // This is a brand new occourance, add it to the song's document
                     console.log(chalk.greenBright(`spotted "${item.title}" (${item.playlistid}): ${readableDate}`))
@@ -48,7 +49,7 @@ async function pullData() {
 
             } else {
                 // Song doesn't exist in MongoDB, so add it along with this playtime
-                console.log(chalk.yellowBright(`discovered "${item.title}" (${item.playlistid})`));
+                console.log(chalk.yellowBright(`discovered "${item.title}" (${item.playlistid}): ${readableDate}`));
                 return db.insertOne({
                     id: item.playlistid,
                     title: item.title,
